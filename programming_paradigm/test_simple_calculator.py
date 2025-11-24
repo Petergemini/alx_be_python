71cdb76e86d7a81d96b7d4242f04c4dcfe0edb58
petergemini <petergemini7764@gmail.com>
2025-11-24 12:58:20 +0300

    ALX unit tests for SimpleCalculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,31 @@
+
+# test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Create a SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the add method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-5, -7), -12)
+
+    def test_subtraction(self):
+        """Test the subtract method."""
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-3, -7), 4)
+        self.assertEqual(self.calc.subtract(10, 20), -10)
+
+    def test_multiplication(self):
+        """Test the multiply method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual
